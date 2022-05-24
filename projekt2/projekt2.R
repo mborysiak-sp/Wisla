1f5160bcd008a936bc8f354b2a195fb36d259c1a
Marcin Borysiak <35338647+mborysiak-sp@users.noreply.github.com>
2022-05-24 21:45:20 +0200

    proj2 mapy2

--- projekt2/projekt2.R
+++ projekt2/projekt2.R
@@ -111,8 +111,8 @@ for (station in formatted)
 
   thresholds[[length(thresholds) + 1]] <- u
 
-  k20 <- 20 * 31 * 24 * 6
-  k50 <- 50 * 31 * 24 * 6
+  k20 <- 20
+  k50 <- 50
   fit <- gpdFit(data, threshold = u)
   r <- tryCatch({
     rl20 <- gpdRl(fit, period = k20, method = "profile", plot = FALSE)$Estimate
@@ -151,13 +151,13 @@ library(maps)
 
 stations <- formatted
 
-for (i in 1:(min(length(stations), length(x50pp1)))) {
-  stations[[i]][[3]] <- x50pp1[[i]]
+for (i in 1:(min(length(stations), length(x20pp1)))) {
+  stations[[i]][[3]] <- x20pp1[[i]]
 }
 
-thresholds_sorted <- sort(unlist(x50pp1), decreasing = FALSE)
+thresholds_sorted <- sort(unlist(x20pp1), decreasing = FALSE)
 stations_sorted <- stations[order(sapply(stations, "[[", 3))]
-png("Figures/proj2/mapa_x50.png", width = 2048, height = 2048)
+png("Figures/proj2/mapa_progi_pot.png", width = 2048, height = 2048)
 map('world', 'poland', fill = T, col = 'gray')
 rbPal <- colorRampPalette(c('blue', 'yellow', 'red'))
 step <- 7
@@ -169,11 +169,9 @@ for (station in stations_sorted) {
   lon <- station[[1]]$lon
   lat <- station[[1]]$lat
   points(lon, lat, pch = 20, col = station[[4]], cex = 10)
-  # par(ps=5)
-  # text(lon, lat, station[[3]])
 }
 dev.off()
-png("Figures/proj2/legenda_x50.png", width = 2048, height = 2048)
+png("Figures/proj2/legenda_progi_pot.png", width = 2048, height = 2048)
 color.bar(rbPal(step), thresholds_sorted[[1]], thresholds_sorted[[length(thresholds_sorted)]], nticks = step)
 dev.off()
 
@@ -183,52 +181,104 @@ library(gamlss)
 load(file = "data/proj2/Selected_Temp_August.Rdata")
 x20pp2 <- list()
 x50pp2 <- list()
+r_list <- list()
 parGEVr <- NULL
-for(ii in 1:length(formatted)){
-  t1<-Sys.time()
-  temps <- formatted[[ii]][[2]][complete.cases(formatted[[ii]][[2]]), ]
+for (ii in 1:length(formatted)) {
+  t1 <- Sys.time()
+  temps <- formatted[[ii]][[2]][complete.cases(formatted[[ii]][[2]]),]
   r <- 10
-  os10 <- matrix(NA,nrow=length(2008:2018),ncol=r)
+  os10 <- matrix(NA, nrow = length(2008:2018), ncol = r)
   count <- 1
-  for(i in 2008:2018){
+  for (i in 2008:2018) {
     datar <- temps[temps$year == i, 5]
     datar <- datar[!is.na(datar)]
-    x <- sort(datar,decreasing = TRUE)
+    x <- sort(datar, decreasing = TRUE)
     y <- x[1:r]
     os10[count,] <- y
     count <- count + 1
   }
   parGEVr <- matrix(NA, nrow = 10, ncol = 3)
-  B <- 350
+  B <- 100
   r <- tryCatch({
-    test10 <- gevrSeqTests(os10, method="pbscore", bootnum=B)
+    test10 <- gevrSeqTests(os10, method = "pbscore", bootnum = B)
 
     p_FS <- rev(test10$ForwardStop)
 
-    length(Filter(function(x) x > 0.05, p_FS))
-    }, error = function(cond) {
+    r <- length(Filter(function(x) x > 0.05, p_FS))
+  }, error = function(cond) {
     return(NA)
   })
   x20 <- NA
   x50 <- NA
-  if(!is.na(r)){
-    if(r > 0){
-      osr <- os10[,1:r]
-      fit <- gevrFit(osr,method="mle")
+  if (!is.na(r)) {
+    if (r > 0) {
+      osr <- os10[, 1:r]
+      fit <- gevrFit(osr, method = "mle")
 
       parGEVr[r,] <- fit$par.ests
 
-      x20 <- gevrRl(fit,20)$Estimate
-      x50 <- gevrRl(fit,50)$Estimate
+      x20 <- gevrRl(fit, 20)$Estimate
+      x50 <- gevrRl(fit, 50)$Estimate
+      if (x20 > 40) {
+        x20 <- NA
+      }
+      if (x50 > 40) {
+        x50 <- NA
+      }
     }
   }
+
   x20pp2[[length(x20pp2) + 1]] <- x20
   x50pp2[[length(x50pp2) + 1]] <- x50
-
+  r_list[[length(r_list) + 1]] <- r
 
   print(round(100 * ii / length(formatted), 1))
-  t2<-Sys.time()
-  print(t2-t1)
+  t2 <- Sys.time()
+  print(t2 - t1)
 }
-save(x20pp2, x50pp2, file="data/proj2/BMM_Temp_August.Rdata")
+save(x20pp2, x50pp2, file = "data/proj2/BMM_Temp_August100.Rdata")
+load(file = "data/proj2/BMM_Temp_August.Rdata")
 
+color.bar <- function(lut, min, max = -min, nticks = 11, ticks = seq(min, max, len = nticks), title = '') {
+  margin <- 20
+  par(mar = c(margin, margin, margin, margin))
+  scale <- (length(lut) - 1) / (max - min)
+  plot(c(0, 10), c(min, max), type = 'n', bty = 'n', xaxt = 'n', xlab = '', yaxt = 'n', ylab = '', main = title)
+  axis(2, ticks, las = 1, cex.axis = 5, font = 2,)
+  for (i in 1:(length(lut) - 1)) {
+    y <- (i - 1) / scale + min
+    rect(0, y, 10, y + 1 / scale, col = lut[i], border = NA)
+  }
+}
+
+library(maps)
+
+stations <- formatted
+
+for (i in 1:(min(length(stations), length(x50pp2)))) {
+  stations[[i]][[3]] <- x50pp2[[i]]
+}
+
+thresholds_sorted <- sort(unlist(x50pp2), decreasing = FALSE)
+stations_sorted <- stations[order(sapply(stations, "[[", 3))]
+png("Figures/proj2/mapa_bmm50.png", width = 2048, height = 2048)
+map('world', 'poland', fill = T, col = 'gray')
+rbPal <- colorRampPalette(c('blue', 'yellow', 'red'))
+step <- 7
+col <- rbPal(step)[as.numeric(cut(thresholds_sorted, breaks = step))]
+for (i in 1:(length(stations_sorted))) {
+  stations_sorted[[i]][[4]] <- col[i]
+}
+for (station in stations_sorted) {
+  lon <- station[[1]]$lon
+  lat <- station[[1]]$lat
+  if (is.na(station[[3]])) {
+    points(lon, lat, pch = 20, cex = 10)
+  } else {
+    points(lon, lat, pch = 20, col = station[[4]], cex = 10)
+  }
+}
+dev.off()
+png("Figures/proj2/legenda_bmm50.png", width = 2048, height = 2048)
+color.bar(rbPal(step), thresholds_sorted[[1]], thresholds_sorted[[length(thresholds_sorted)]], nticks = step)
+dev.off()
